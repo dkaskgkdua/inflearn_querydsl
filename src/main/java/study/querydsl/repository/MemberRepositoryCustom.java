7f4e5ccc51199da32615006c00d1ff12868fa6d0
SongMinJun <dkaskgkdua@gmail.com>
2021-06-01 23:17:50 +0900

    사용자 정의 리포지토리
    - 커스텀 repository을 jpa repository에서 사용

--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -1,8 +1,12 @@
 package study.querydsl.repository;
 
 
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+
 import java.util.List;
 
 public interface MemberRepositoryCustom {
 //    List<Member> findMemberCustom();
+    List<MemberTeamDto> search(MemberSearchCondition condition);
 }
