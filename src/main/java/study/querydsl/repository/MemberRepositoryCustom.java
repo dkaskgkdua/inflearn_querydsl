800299872bc1ddd5a133ae1784fa43ee2e71af5a
SongMinJun <dkaskgkdua@gmail.com>
2021-06-04 23:51:19 +0900

    querydsl 페이징 연동

--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -1,6 +1,8 @@
 package study.querydsl.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 
@@ -9,4 +11,6 @@ import java.util.List;
 public interface MemberRepositoryCustom {
 //    List<Member> findMemberCustom();
     List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
